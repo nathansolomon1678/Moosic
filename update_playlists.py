53d4d8f00d8cd0cf5c7b2b4daa72ab9a65055493
Nathan Solomon <nathansolomon1678@gmail.com>
2021-01-17 02:18:27 -0800

    BLEARGHSLSGLKEGW:SMNK

--- update_playlists.py
+++ update_playlists.py
@@ -9,9 +9,14 @@ from typing import List, Dict, Any
 
 with open('api_key_stuff.json') as file:
     info = json.load(file)
-    os.environ["SPOTIPY_CLIENT_ID"]     = info['Client ID']
-    os.environ["SPOTIPY_CLIENT_SECRET"] = info['Client Secret']
-s = spotipy.Spotify(auth_manager=spotipy.oauth2.SpotifyClientCredentials())
+    token = spotipy.util.prompt_for_user_token(
+        info['Username'],
+        info['Scope'],
+        info['Client ID'],
+        info['Client Secret'],
+        info['Redirect URI']
+    )
+s = spotipy.Spotify(auth=token)
 
 def get_playlist(playlist_id: str, sort=True) -> List[Dict[str, Any]]:
     """ Returns useful information for each song on a playlist """
@@ -38,8 +43,46 @@ def get_playlist(playlist_id: str, sort=True) -> List[Dict[str, Any]]:
         songs.sort(key = lambda song: song['Title'])
     return songs
 
+def playlist_name(id):
+    """ Returns the name of a playlist whose ID is given """
+    return s.playlist(id, fields='name')['name']
+
+def add_songs(playlist, songs):
+    """ Given a playlist ID & a list of song IDs, adds all those songs to the playlist """
+    # Can only add 100 at a time, so split songs into chunks of 100
+    while len(songs) >= 100:
+        s.playlist_add_items(playlist, songs[:100])
+        del songs[:100]
+    # Since the number of songs likely wasn't divisible by 100, add the remaining ones
+    s.playlist_add_items(playlist, songs)
+
+def copy(main, feeders):
+    """ Copies all unique songs from feeder playlists to a main playlist """
+    main_playlist_name = playlist_name(main)
+    songs = {song['ID'] for song in get_playlist(main)}
+    for feeder in feeders:
+        feeder_name = playlist_name(feeder)
+        new_songs = {song['ID'] for song in get_playlist(feeder)} - songs
+        add_songs(main, list(new_songs))
+        songs = songs.union(new_songs)
+        print(f'Copied {len(new_songs)} new songs from {feeder_name} to {main_playlist_name}')
+
+# Dictionary of main playlists to feeders for each of those
+# For example, the ID of a family playlist is the key to the IDs of individual playlists for each family member
+feeder_playlists = {
+    '6PR9WVuDfnp98ZxvqTvdyy': [
+        '6vxJkWv5QjBCWuMUU11Yu0',
+        '496qzGOJjyhvnAkGS7Z5nR',
+        '0nIOYMhTBQDrGwrvCc6TUW',
+        '6vDwFEEeqByv0EdEpgX0LR',
+    ],
+}
+
+for main, feeders in feeder_playlists.items():
+    copy(main, feeders)
+
 playlists_to_version_control = {
-    '496qzGOJjyhvnAkGS7Z5nR',  # For now, I'll just be version controlling my main playlist
+    '496qzGOJjyhvnAkGS7Z5nR',
 }
 
 os.system('rm -rf playlists')
@@ -50,4 +93,4 @@ for playlist in playlists_to_version_control:
         file.write(f'{str(song)}\n')
     file.close()
 
-print("Don't forget to commit and push these updates!  :)")
+print('Don\'t forget to commit and push these updates!  :)')
