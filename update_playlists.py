00c97b7e1c589979f76c3278b824c7dcdcb0a258
Nathan Solomon <nathansolomon1678@gmail.com>
2021-01-23 13:37:39 -0800

    :)))))

--- update_playlists.py
+++ update_playlists.py
@@ -5,7 +5,8 @@ Those changes are saved in the playlists/ directory """
 import json
 import os
 import spotipy
-from typing import List, Dict, Any
+import re
+from typing import List, Dict, Any, Tuple
 
 with open('api_key_stuff.json') as file:
     info = json.load(file)
@@ -18,11 +19,24 @@ with open('api_key_stuff.json') as file:
     )
 s = spotipy.Spotify(auth=token)
 
+def same_song(song1: Dict[str, Any], song2: Dict[str, Any]) -> bool:
+    """ Given two songs (each as dictionaries containing the title and artists),
+    returns whether the songs are the same, even if they have different IDs """
+    # List of all artists for each song:
+    artists: List[List[str]] = [song['Artists'] for song in [song1, song2]]
+    artists_overlap: bool = len(set(artists[0]).intersection(set(artists[1]))) > 0
+    # The pattern ' - ' is usually followed by 'Remastered <year>' or 'Radio edit' or something
+    titles: List[str] = [song['Title'].split(' - ')[0] for song in [song1, song2]]
+    # Also, anything enclosed in parentheses is unnecessary
+    titles = [re.sub('\(.*\)', '', title) for title in titles]
+    return artists_overlap and titles[0] == titles[1]
+
 def get_playlist(playlist_id: str, sort=True) -> List[Dict[str, Any]]:
     """ Returns useful information for each song on a playlist """
     songs = []
     offset = 0
-    while True:  # Retrieve 100 songs at a time until there aren't any left
+    # Retrieve 100 songs at a time until there aren't any left
+    while True:
         # The offset tells it which index to start at
         new_songs = s.playlist_items(playlist_id, offset=offset, limit=100)['items']
         offset += 100
@@ -43,7 +57,7 @@ def get_playlist(playlist_id: str, sort=True) -> List[Dict[str, Any]]:
         songs.sort(key = lambda song: song['Title'])
     return songs
 
-def playlist_name(id):
+def get_playlist_name(id):
     """ Returns the name of a playlist whose ID is given """
     return s.playlist(id, fields='name')['name']
 
@@ -56,14 +70,34 @@ def add_songs(playlist, songs):
 
 def copy(main, feeders):
     """ Copies all unique songs from feeder playlists to a main playlist """
-    main_playlist_name = playlist_name(main)
-    songs = {song['ID'] for song in get_playlist(main)}
+    main_playlist_name = get_playlist_name(main)
+    songs = get_playlist(main)
     for feeder in feeders:
-        feeder_name = playlist_name(feeder)
-        new_songs = {song['ID'] for song in get_playlist(feeder)} - songs
-        add_songs(main, list(new_songs))
-        songs = songs.union(new_songs)
-        print(f'Copied {len(new_songs)} new songs from "{feeder_name}" to "{main_playlist_name}"')
+        feeder_name = get_playlist_name(feeder)
+        new_songs = []
+        for new_song in get_playlist(feeder):
+            if True not in [same_song(new_song, existing_song) for existing_song in songs]:
+                new_songs.append(new_song)
+                songs.append(new_song)
+        add_songs(main, [song['ID'] for song in new_songs])
+        print(f'\033[0;1mCopied {len(new_songs)} new songs from "{feeder_name}" to "{main_playlist_name}":')
+        for song in new_songs:
+            print(f'\033[0;32m{song}')
+
+def warn_of_dupes(songs: List[Dict[str, Any]], playlist_name: str) -> None:
+    """ Given a list of songs, prints any that appear to be duplicates """
+    # It would be really nice to use sets here, but dictionaries are unhashable, so we can't
+    unique_songs = []
+    # Instead of printing dupes, store them in a list, to be counted and then printed
+    dupes_to_print = []
+    for new_song in songs:
+        for existing_song in unique_songs:
+            if same_song(new_song, existing_song):
+                dupes_to_print.append(f'\033[0;31m{new_song}\033[0;35m (similar to\n{existing_song})\n')
+                break
+        unique_songs.append(new_song)
+    print(f'\033[0;1mFound {len(dupes_to_print)} duplicate songs in "{playlist_name}":')
+    for thing_to_print in dupes_to_print: print(thing_to_print)
 
 # Dictionary of main playlists to feeders for each of those
 # For example, the ID of a family playlist is the key to the IDs of individual playlists for each family member
@@ -73,6 +107,9 @@ feeder_playlists = {
         '496qzGOJjyhvnAkGS7Z5nR',
         '0nIOYMhTBQDrGwrvCc6TUW',
         '6vDwFEEeqByv0EdEpgX0LR',
+        '7A3XejLvWvSmIX2yGQPCJ5',
+        '66IDHTnc0e2RkqsMSyFHu3',
+        '1LlrlIzlZVzNEqz1FOeuZh',
     ],
 }
 
@@ -87,9 +124,12 @@ playlists_to_version_control = {
 os.system('rm -rf playlists')
 os.mkdir('playlists')
 for playlist in playlists_to_version_control:
-    file = open(f'playlists/{playlist}', 'w')
-    for song in get_playlist(playlist):
+    songs = get_playlist(playlist)
+    playlist_name = get_playlist_name(playlist)
+    warn_of_dupes(songs, playlist_name)
+    file = open(f'playlists/{playlist_name}', 'w')
+    for song in songs:
         file.write(f'{str(song)}\n')
     file.close()
 
-print('Don\'t forget to commit and push these updates!  :)')
+print('\033[0;36mDon\'t forget to commit and push these updates!  :)')
