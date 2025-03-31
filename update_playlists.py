75786ec22d77415c3a033f2079a8487dd968c9fa
Nathan Solomon <nathansolomon1678@gmail.com>
2025-03-31 05:50:49 -0700

    New playlist for music with non-English lyrics

--- update_playlists.py
+++ update_playlists.py
@@ -37,6 +37,7 @@ playlists_to_version_control = {
     '4PUiJEVFZoBpouVuKcJ5GR',
     '736Es5MNOObGsEYWbJ5XYy',
     '4g2fm0Vbp0f6c0A08zGL2Z',
+    '6PUbvtl4L1YqXginld0oJF',
 }
 
 os.system('rm -rf playlists')
